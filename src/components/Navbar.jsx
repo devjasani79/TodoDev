55519c892ec7de06f19174a368012497a24573c2
Dev Jasani <104269899+devjasani79@users.noreply.github.com>
2025-02-05 16:17:50 +0530

    Update Navbar.jsx
    logo adjusted

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,8 @@ const Navbar = () => {
   return (
     <nav className='flex justify-between bg-slate-900 text-white py-2' >
       <div className="logo">
-        <span className='font-bold text-xl mx-9'>Todo.Dev</span>
+       
+        <div className='font-bold flex gap-2 text-xl mx-9'> <img src="public/logo.png" className='w-10' alt="" />Todo.Dev</div>
       </div>
       <ul className="flex gap-8 mx-9">
         <li className='cursor-pointer hover:font-bold transition-all'>Home</li>
